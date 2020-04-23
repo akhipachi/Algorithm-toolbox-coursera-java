661adcad4370371d5e94d6f14c1bc75a2424001b
akhipachi <32668096+akhipachi@users.noreply.github.com>
2020-04-23 16:03:40 +0530

    Add files via upload

---
+++ week1_programming_challenges/2_maximum_pairwise_product/MaxPairwiseProduct.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class MaxPairwiseProduct {
+    static BigInteger getMaxPairwiseProduct(int[] numbers) {
+        BigInteger max_product = new BigInteger("0");
+        int n = numbers.length;
+
+        Arrays.sort(numbers);
+        max_product=BigInteger.valueOf(numbers[n-1]).multiply(BigInteger.valueOf(numbers[n-2]));
+
+        return max_product;
+    }
+
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner(System.in);
+        int n = scanner.nextInt();
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+        System.out.println(getMaxPairwiseProduct(numbers));
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(InputStream stream) {
+            try {
+                br = new BufferedReader(new
+                    InputStreamReader(stream));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+}
